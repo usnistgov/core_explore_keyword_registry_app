76ab045750a9d7a8163998e38d60640c17228514
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-05-09 09:27:35 -0400

    Initial commit
    Change-Id: I7354b97c665480ad75c02f2bacb25c82b162423c

---
+++ core_explore_keyword_registry_app/urls.py
@@ -0,0 +1,16 @@
+""" Url router for the explore keyword registry application
+"""
+
+from django.conf.urls import url
+from core_explore_keyword_app.views.user import views as user_views, ajax as user_ajax
+
+urlpatterns = [
+    url(r'^$', user_views.KeywordSearchView.as_view(),
+        name='core_explore_keyword_app_search'),
+    url(r'^(?P<query_id>\w+)$', user_views.KeywordSearchView.as_view(),
+        name='core_explore_keyword_app_search'),
+    url(r'^get-persistent-query-url$', user_ajax.CreatePersistentQueryUrlKeywordView.as_view(),
+        name='core_explore_keyword_get_persistent_query_url'),
+    url(r'^results-redirect/(?P<persistent_query_id>\w+)', user_views.ResultQueryRedirectKeywordView.as_view(),
+        name='core_explore_keyword_results_redirect'),
+]
