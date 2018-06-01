af1d9b61e3c37792b43c247270f40b5740f6d7d2
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-06-01 09:53:31 -0400

    feat: Custom rendering of results.
    Change-Id: I29e84ad28bf827d1061b2cee99a9a9bd79bf7f17

---
+++ core_explore_keyword_registry_app/static/core_explore_keyword_registry_app/user/js/search/search.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+     $(".description").shorten({
+        showChars: 350,
+        ellipsesText: "",
+        moreText: "... show more",
+        lessText: " show less",
+    });
+});
