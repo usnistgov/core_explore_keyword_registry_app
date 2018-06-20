a89ac02c74e123e8260d3628b84900d42a4f0f57
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-06-20 10:08:06 -0400

    feat: Use the refinements in the search
    Change-Id: I3b541b26106ce36854a7134178a661bb6e5c3012

--- core_explore_keyword_registry_app/views/user/views.py
+++ core_explore_keyword_registry_app/views/user/views.py
@@ -1,7 +1,12 @@
 """Core Explore Keyword App views
 """
+import json
+
+from core_explore_common_app.components.query import api as query_api
 from core_explore_keyword_app.views.user.views import KeywordSearchView
 from core_explore_keyword_registry_app.views.user.forms import RefinementForm
+from core_main_app.commons.exceptions import DoesNotExist
+from core_main_registry_app.utils.refinement.mongo_query import build_refinements_query
 
 
 class KeywordSearchRegistryView(KeywordSearchView):
@@ -31,8 +36,44 @@ class KeywordSearchRegistryView(KeywordSearchView):
 
         """
         context = super(KeywordSearchRegistryView, self)._post(request)
-        # TODO: refill the form with selected values
-        context.update({'refinement_form': RefinementForm(request.POST)})
+        # get refinement form
+        refinement_form = RefinementForm(data=request.POST)
+        # get query_id and error from the context
+        error = context.get("error", None)
+        query_id = context.get("query_id", None)
+        # validate form, test if no errors occurred in the parent treatment and query_id exists
+        if refinement_form.is_valid() and error is None and query_id is not None:
+            refinements = []
+
+            # get selected refinements (categories)
+            for refinement_name, selected_categories in refinement_form.cleaned_data.iteritems():
+                if len(selected_categories) > 0:
+                    # Add categories ids
+                    refinements.append([x.id for x in selected_categories])
+
+            # generate query
+            if len(refinements) > 0:
+                try:
+                    # get refinement query
+                    refinement_query = build_refinements_query(refinements)
+                    # if we have a refinement query
+                    if len(refinement_query.keys()) > 0:
+                        # get query from database
+                        query = query_api.get_by_id(query_id)
+                        content = json.loads(query.content)
+                        # update query content
+                        content.update(refinement_query)
+                        query.content = json.dumps(content)
+                        # save query
+                        query_api.upsert(query)
+                except DoesNotExist:
+                    error = "An unexpected error occurred while retrieving the query."
+                    context.update({'error': error})
+                except Exception, e:
+                    error = "An unexpected error occurred: {}.".format(e.message)
+                    context.update({'error': error})
+
+        context.update({'refinement_form': refinement_form})
         return context
 
     def _load_assets(self):
